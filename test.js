962113282e26972a4f06b029932a910edd732329
Paul <debiasi.paul@gmail.com>
2020-09-11 14:29:06 +0200

    finishing test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+// Exercise I
+
+var x = 888;
+var xx = x;
+
+function timesTwo (num){
+	return num * 2
+}
+ timesTwo(x)
+
+ var numbers = [x,xx];
+
+ for(i = 0; i<numbers.length;i++){
+return 
+ }
+var numbers ={
+	way = xx
+}
