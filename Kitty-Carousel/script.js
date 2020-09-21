d1bca4432b3c07f22370a7f945bf175458ee3bed
Paul <debiasi.paul@gmail.com>
2020-09-21 07:22:14 +0200

    finishing Carousel

--- Kitty-Carousel/script.js
+++ Kitty-Carousel/script.js
@@ -1,31 +1,46 @@
 (function () {
     var kitties = document.querySelectorAll(".kitty-container img");
+    var dots = document.getElementsByClassName("dot");
+    var cur = 0;
+    var timer;
+    // var isCurrentlyAnimating;
+    // console.log('dots', dots);
+    timer = setInterval(moveKitties, 5000); // what is the output of setTimeout?
 
+    for (var i = 0; i < dots.length; i++) {
+        // console.log('dots', dots[i]);
+        dots[i].addEventListener("click", function (e) {
+            // clearTimeout(timer);
+            // console.log(e.target); // don't understand
+            // console.log(e.target.id.slice(3)); // don't understand
+            console.log("dots[i]", dots[i]);
+            if (dots[i] === e.target.classList) {
+                console.log(i);
+            }
+        });
+    }
+
+    // console.log('kitties', kitties);
     function moveKitties() {
-        setInterval(moveKitties, 500);
-        // to remove a kitty from the screen...
-        kitties[0].classList.remove("onscreen");
-        kitties[0].classList.add("offscreen-left");
+        kitties[cur].classList.remove("onscreen");
+        dots[cur].classList.remove("on");
+        kitties[cur].classList.add("offscreen-left");
 
-        // to pull the next kitty onscreen...
-        kitties[1].classList.add("onscreen");
-    }
-
-    var i = 0;
-    var img = [];
-
-    function changeImg() {
-        document.kitties = img[i];
-        if (i > img.length - 1);
-        {
-            ++i;
+        if (cur < kitties.length) {
+            cur++;
         }
+        if (cur === kitties.length) {
+            cur = 0;
+        }
+
+        kitties[cur].classList.add("onscreen");
+        dots[cur].classList.add("on");
     }
 
-    setTimeout(moveKitties, 3000);
-
-    document.addEventListener("transitionend", function () {
-        if (classList === "offscreen-left") {
+    document.addEventListener("transitionend", function (e) {
+        if (e.target.classList.contains("offscreen-left")) {
+            e.target.classList.remove("offscreen-left");
         }
     });
 })();
+s
